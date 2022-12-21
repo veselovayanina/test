de7ec7c878a12ec18836a6efa3a648e8d1ec6fd2
veselovayanina <117366381+veselovayanina@users.noreply.github.com>
2022-12-20 19:53:26 -0500

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,29 @@
+//1. Obtener la lista de elementos de etiqueta <p> y mostrarlos en un alert de JavaScript.
+
+<body>
+    <script language = JavaScript>
+    {var arr = document.getElementById("p").value;
+    }
+
+    <p>Hola!</p>
+
+    alert("Hello " + significa +"arr");
+
+    </script>
+</body>
+
+
+//const p = [
+//    'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+//console.log('Prueba = ' + p + '.');
+
+//2. Añadir un nuevo elemento <p> con el texto “Texto del nuevo párrafo” y que sea hijo del body.
+
+//<body>
+//<p”alert(`Un mensaje de prueba´)”>Un párrafo de texto.</p>
+//</body>
+
+
+//<body>
+    function = a.push('Texto del nuevo párrafo');
+//</body>
